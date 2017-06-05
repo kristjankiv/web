3119813ad9632e849b6f3afbc59227cd774a7e09
kristjankiv <kristjan.kivastik@khk.ee>
2017-06-05 12:18:16 +0300

    Improved error showing

---
+++ acts/login_do.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristjan.kivastik
+ * Date: 31.05.2017
+ * Time: 11:07
+ */
+// võtame kätte vormi poolt edastatud andmed
+$username = $http->get('kasutaja');
+$passwd = $http->get('parool');
+//koostame päringu kasutaja kontrollimiseks andmebaasis
+$sql = 'SELECT * from user'.
+    ' where username='.fixDb($username).
+    ' AND password='.fixDb(md5($passwd));
+$res = $db->getArray($sql);
+// Teeme päringu tulemuse kontrolli
+if($res == false){
+    // loome veateade ja paneme see sessiooni
+    $sess->set('error', 'Probleem sisselogimisega');
+    // siis tuleb kasutaja suunata tagasi sisselogimisvormi
+    $link = $http->getLink(array('act' => 'login'));
+    $http->redirect($link);
+}else{
+    //Siis tuleb avada kasutaja sessioon
+    $sess->createSession($res[0]);
+    // Tuleb suunata kasutajad pealehele
+    //Kui ma väljastan kasutajaandmed sessiooni kontrolliks
+    $http->redirect();
+}
