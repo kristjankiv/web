095667fd14f3c49e22db09bd0aa9da8f76d305ac
kristjankiv <kristjan.kivastik@khk.ee>
2017-06-05 12:23:03 +0300

    Create lang_en.php

---
+++ lang/lang_en.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristjan.kivastik
+ * Date: 31.05.2017
+ * Time: 11:13
+ */
+// /lang/lang_en.php
+$_trans = array(
+    'Esimene' => 'First',
+    'Teine' => 'Second',
+    'Minu lehe pealkiri' => 'My page heading',
+    'Logi välja' => 'Logout',
+    'Kasutajanimi' => 'Username',
+    'Parool' => 'Password',
+    'Logi sisse' => 'Login',
+    'Viga sisselogimisel' => 'Login error'
+);
+?>
