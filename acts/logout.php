fbab6a6abf176e6a4bfcf43b2088ee7536bf776b
kristjankiv <kristjan.kivastik@khk.ee>
2017-06-05 12:18:35 +0300

    logout

---
+++ acts/logout.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristjan.kivastik
+ * Date: 31.05.2017
+ * Time: 11:08
+ */
+$sess->delSession(); // kustutame olemas olev sessioon
+$http->redirect();// suuname pealehele
+?>
