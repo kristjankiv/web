e3e6de8f9ca6963207193cd4faeb47ce845b2840
kristjankiv <kristjan.kivastik@khk.ee>
2017-06-05 12:05:46 +0300

    Adding language bar to page + fixes

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristjan.kivastik
+ * Date: 22.03.2017
+ * Time: 15:14
+ */
+//votame konfiguratsiooni kasutusele
+require_once 'conf.php';
+//pealehe sisu
+echo '<h1>Veebiprogrammeerimise esileht </h1>';
+// valmistame peatemplate objekti.
+$main_tmpl = new template('main');
+// keele kontrollimine
+require_once('lang.php');
+// valmistame paarid malli element => väärtus
+$main_tmpl->set('user', 'Kasutajanimi');
+$main_tmpl->set('title', 'Pealeht');
+// kutsume menüü tööle testimiseks
+require_once 'menu.php';
+// tõstsime vaikimisi väljundi default tegevuse sisse
+require_once 'act.php';
+$main_tmpl->set('site_title', 'Veebiprogrammeerimise kursus');
+// kontrollin antud objekti sisu
+echo $main_tmpl->parse();
+?>
